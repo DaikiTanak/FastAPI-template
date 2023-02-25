afe72b94a2fe701406f756ed2d5abe45cb411dd8
田中大貴 <daiki@tanakadaikinoMacBook-Air.local>
2023-02-25 15:29:19 +0900

    fix flake8

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ api_service = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+
 @api_service.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
