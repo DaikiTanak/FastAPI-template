476ee161dd747ab9707342cb21adc8775a646aed
DaikiTanak <daiki@tanakadaikinoMacBook-Air.local>
2023-02-25 19:01:16 +0900

    gunicorn

--- app/main.py
+++ app/main.py
@@ -2,14 +2,14 @@ from typing import Optional
 
 from fastapi import FastAPI
 
-api_service = FastAPI()
+app = FastAPI()
 
 
-@api_service.get("/")
+@app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@api_service.get("/items/{item_id}")
+@app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,8 @@
 from fastapi.testclient import TestClient
 
-from app.main import api_service
+from app.main import app
 
-client = TestClient(api_service)
+client = TestClient(app)
 
 
 def test_read_main():
